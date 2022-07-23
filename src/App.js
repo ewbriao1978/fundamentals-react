237fd38fadfe36612fb1aea71ed78354caccd38d
ewbriao1978 <eduardo.briao@gmail.com>
2022-07-23 00:00:29 -0300

    Setting components and parameters

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import FirstComponent from './components/FirstComponent';
+import SecondComponentParameter from './components/SecondComponentParameter';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+            <FirstComponent />
+            <p>Página Principal</p>
+            <SecondComponentParameter index="2" other="algum"/>
+
+     </div>
   );
 }
 
