78cd0703789d20d3e74ee3a567a3225941d33ff6
ewbriao1978 <eduardo.briao@gmail.com>
2022-07-23 21:50:22 -0300

    Add List Item and ItemComponent

--- src/App.js
+++ src/App.js
@@ -2,24 +2,32 @@ import './App.css';
 import FirstComponent from './components/FirstComponent';
 import ParentComponent from './components/ParentComponent';
 import SecondComponentParameter from './components/SecondComponentParameter';
+import ListItems from './components/List/ListItems';
+import products from './data/products';
 
 function App() {
+  
   return (
     <div className="App">
 
             <FirstComponent />
+
             <p>Página Principal</p>
+
             <SecondComponentParameter index="2" other="algum"/>
             <SecondComponentParameter index="4" other="mais algum"/>
+
             <ParentComponent>
               <ul>
                 <li>item 1</li>
                 <li>item 2</li>
                 <li>...</li>
               </ul>
-
-
             </ParentComponent>
+
+            <p>My List of Items</p>
+
+            <ListItems items = {products}/>
 
 
      </div>
---
+++ src/components/List/ItemComponent.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./ItemComponent.css";
+
+export default props => {
+    return (
+
+        <li>
+            <div className='expense-item__description'>
+            <h2>{props.name}</h2>
+            <div className='expense-item__price'>${props.price}</div>
+            </div>
+         </li>
+    );
+ 
+}
